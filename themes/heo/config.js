066efb1ae1fc3af366874e45b6aed74a552d546b
xuhxjx <xuhxjxhk@gmail.com>
2025-09-21 18:06:34 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://blog.363689.xyz' },
+    { title: '我的配置中心博文', url: 'https://www.notion.so' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -19,11 +19,11 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_2: '思维认知',
+  HEO_HERO_TITLE_3: 'blog.363689.xyz',
+  HEO_HERO_TITLE_4: 'hxjx\'s blog',
+  HEO_HERO_TITLE_5: '记录心得，分享技巧',
+  HEO_HERO_TITLE_LINK: 'https://bg.363689.xyz',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -51,9 +51,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/xuhxjx',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://blog.363689.xyz',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
